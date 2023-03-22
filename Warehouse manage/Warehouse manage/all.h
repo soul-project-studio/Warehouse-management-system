039ed08c6a5dd775cf86bb05f4bcbe9131d56212
qingmolixian <31708021@qq.com>
2023-03-22 23:08:40 +0800

    1

--- Warehouse manage/Warehouse manage/all.h
+++ Warehouse manage/Warehouse manage/all.h
@@ -5,5 +5,15 @@
 #include<iomanip>
 #pragma warning(disable:4996)
 using namespace std;
+
+struct goods
+{
+	char num[13];
+	char name[20];
+	int num;
+	double price;
+	double totalValue;
+};
+
 void menu0();   //一级菜单
 void menu3();   //选项3下的二级菜单
