0fd871f1dc16e0ac47e33638c72d326b31473859
qingmolixian <31708021@qq.com>
2023-03-22 22:21:42 +0800

    1

---
+++ head/all.h
@@ -0,0 +1,9 @@
+#include<cstdio>
+#include<cmath>
+#include<vector>
+#include<iostream>
+#include<iomanip>
+#pragma warning(disable:4996)
+using namespace std;
+void menu0();   //一级菜单
+void menu3();   //选项3下的二级菜单
