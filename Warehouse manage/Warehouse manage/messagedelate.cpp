c251c0a65d8021385f0967fe6f43db26b15414ac
hongbai747 <15029762669@139.com>
2023-03-26 23:12:18 +0800

    1

--- Warehouse manage/Warehouse manage/messagedelate.cpp
+++ Warehouse manage/Warehouse manage/messagedelate.cpp
@@ -1,12 +1,13 @@
 #include"all.h"
 void messagedelate(goods* goods, int n)
 {
-	int i,t;
+	int i;
+	char t[50];
 	printf("输入您要进行出库货物的编码：");
-	scanf("%d",&t);
+	scanf("%s",t);
 	for (i = 0; i < n; i++)
 	{
-		if (t == goods[i].number)
+		if (strcmp(t,goods[i].number))
 		{
 
 		}
