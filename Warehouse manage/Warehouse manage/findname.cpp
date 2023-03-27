fbe7fec5943533dbe36958a5f4e511626d0f06a8
hongbai747 <15029762669@139.com>
2023-03-27 17:16:51 +0800

    1

--- Warehouse manage/Warehouse manage/findname.cpp
+++ Warehouse manage/Warehouse manage/findname.cpp
@@ -4,13 +4,13 @@ void findname(goods* goods, int n)
 	int i, flag = 0;
 	char name[50];
 	printf("请输入要查找的货物名称：");
-	scanf("%s\n", name);
+	scanf("%s", name);
 	for (i = 0; i < n; i++)
 	{
 		if (strcmp(name, goods[i].name) == 0)
 		{
 			printf("恭喜您查询到该货物！\n");
-			printf("编码：%s\n名称：%s\n数量：%d\n单价：%lf\n总价：%lf",goods[i].number, goods[i].name, goods[i].quantity, goods[i].price, goods[i].totalValue);
+			printf("编码：%s\n名称：%s\n数量：%d\n单价：%.2lf\n总价：%.2lf",goods[i].number, goods[i].name, goods[i].quantity, goods[i].price, goods[i].totalValue);
 			flag = 1;
 		}
 
