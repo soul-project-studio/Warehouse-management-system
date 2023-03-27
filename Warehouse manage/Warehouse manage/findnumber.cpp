dcba9c8d807e325d6150d44d5f41ae6ae8838e40
hongbai747 <15029762669@139.com>
2023-03-27 17:15:23 +0800

    11

--- Warehouse manage/Warehouse manage/findnumber.cpp
+++ Warehouse manage/Warehouse manage/findnumber.cpp
@@ -3,14 +3,15 @@ void findnumber(goods* goods, int n)
 {
 	int i,flag=0;
 	char num[11];
-	printf("请输入要查找的货物编码：");
-	scanf("%s\n", num);
+	printf("请输入要查找的货物编码:");
+	scanf("%s",num);
 	for (i = 0; i < n; i++)
 	{
-		if (strcmp(num, goods[i].number) == 0)
+		int cmp = strcmp(num, goods[i].number);
+		if (cmp==0 )
 		{
 			printf("恭喜您查询到该货物！\n");
-			printf("编码：%s\n名称：%s\n数量：%d\n单价：%lf\n总价：%lf",num, goods[i].name,goods[i].quantity,goods[i].price ,goods[i].totalValue);
+			printf("编码：%s\n名称：%s\n数量：%d\n单价：%.2lf\n总价：%.2lf",num, goods[i].name,goods[i].quantity,goods[i].price ,goods[i].totalValue);
 			flag = 1;
 		}
 	}
